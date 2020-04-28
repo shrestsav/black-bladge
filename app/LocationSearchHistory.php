c5cc630a905ce20e777976b8d3f14261be0c18d5
shrestsav <shrestsav@gmail.com>
2020-04-28 12:01:00 +0545

    fsdfsdf

--- app/LocationSearchHistory.php
+++ app/LocationSearchHistory.php
@@ -18,7 +18,7 @@ class LocationSearchHistory extends Model
 
     protected $casts = [
         'favorite'  => 'int',
-        'lattitude' => 'real',
-        'longitude' => 'real',
+        'lattitude' => 'double',
+        'longitude' => 'double',
     ];
 }
