2cb50fd6c421cee23c52dbff6c6c704466111109
shrestsav <shrestsav@gmail.com>
2020-05-06 17:12:09 +0545

    fixes

--- app/Http/Resources/Api/Driver/Driver.php
+++ app/Http/Resources/Api/Driver/Driver.php
@@ -26,6 +26,7 @@ class Driver extends JsonResource
             'photo_src'      => $this->photo_src,
             'title'          => $this->gender,
             'license_no'     => $this->license_no,
+            'vehicle_id'     => $this->vehicle_id,
             'vehicle_number' => $this->vehicle ? $this->vehicle['vehicle_number'] : null,
         ];
     }
